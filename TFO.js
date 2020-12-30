bef41f5ece16865719093582100e2769bc90b4c3
Obsa Siyo <obsasiyo147@gmail.com>
2020-12-30 15:25:50 -0600

    added some dragable code

--- TFO.js
+++ TFO.js
@@ -7,5 +7,58 @@ if (isClickedShow) {
      isClickedShow = false;
 }
 
+
 //changing follows
-var follows = document.querySelector("#sideNav").querySelector(".side-bar-contents").querySelector(".tw-relative").querySelectorAll(".tw-transition");
+var streams = document.querySelector("#sideNav").querySelector(".side-bar-contents").querySelector(".tw-relative").querySelectorAll(".tw-transition");
+var list = document.createElement("UL");
+
+var number = 0
+for ( stream of streams) {
+  stream.id = number;
+  stream.draggable= "true";
+  stream.classList.add("dropzone");
+//   number += 1;
+//   var item = document.createElement("LI");
+//   item.appendChild(stream);
+//   list.appendChild(item);
+}
+
+document.querySelector("#sideNav").querySelector(".side-bar-contents").querySelector(".tw-relative").appendChild(list);
+
+var dragged;
+var id;
+var index;
+var indexDrop;
+var list;
+
+  document.addEventListener("dragstart", ({target}) => {
+      dragged = target;
+      id = target.id;
+      list = target.parentNode.children;
+      for(var i = 0; i < list.length; i += 1) {
+      	if(list[i] === dragged){
+          index = i;
+        }
+      }
+  });
+
+  document.addEventListener("dragover", (event) => {
+      event.preventDefault();
+  });
+
+  document.addEventListener("drop", ({target}) => {
+   if(target.className == "dropzone" && target.id !== id) {
+       dragged.remove( dragged );
+      for(var i = 0; i < list.length; i += 1) {
+      	if(list[i] === target){
+          indexDrop = i;
+        }
+      }
+      console.log(index, indexDrop);
+      if(index > indexDrop) {
+      	target.before( dragged );
+      } else {
+       target.after( dragged );
+      }
+    }
+  });
