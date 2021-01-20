440088464b32c4db9a9c13c5d75f6abd43978f24
Obsa Siyo <obsasiyo147@gmail.com>
2021-01-20 17:43:59 -0600

    Added control Center button and fixed moveablity bug

--- TFO.js
+++ TFO.js
@@ -1,4 +1,4 @@
-// clicking more button
+// clicking "show more" button
 var more = document.querySelector("#sideNav").querySelector(".side-bar-contents").querySelector(".side-nav-show-more-toggle__button").querySelector('[data-a-target="side-nav-show-more-button"]');
 
 // Clicks the button enough times to make sure that "show more" is gone
@@ -13,8 +13,6 @@ do {
 var channels = document.querySelector("#sideNav").querySelector(".side-bar-contents").querySelector(".tw-flex-grow-1");
 channels.id = "dropzone";
 
-
-
 // div for organizer channel
 var organizer = document.createElement("div");
 organizer.classList.add("side-nav-section");
@@ -22,7 +20,6 @@ organizer.classList.add("tw-flex");
 organizer.classList.add("tw-flex-column");
 organizer.setAttribute("aria-label", "Organizer");
 organizer.setAttribute("role", "group");
-
 
 // Header Div
 var headerDiv = document.createElement("div");
@@ -49,10 +46,8 @@ organizer.appendChild(channelHolder);
 //Getting followed
 var streams = document.querySelector("#sideNav").querySelector(".side-bar-contents").querySelector(".tw-relative").querySelectorAll(".tw-transition");
 
-
 // putting varies attributes and moving follwed to created div
 var list = document.createElement("ul");
-
 var number = 0;
 for (stream of streams) {
  
@@ -75,15 +70,12 @@ for (stream of streams) {
 }
 channelHolder.appendChild(list);
 
-
-
-
 // add to side bar Channels
 var ref = document.querySelector("#sideNav").querySelector(".side-bar-contents").querySelector(".side-nav-section");
 channels.insertBefore(organizer, ref);
 
 //removes the show less and header for the followed channel
-ref.remove()
+ref.remove();
 
 
 
@@ -120,15 +112,18 @@ for ( var i = 0; i < nodes.length; i++){
 dropzone.addEventListener("dragover", (ev) => {
     ev.preventDefault();
     console.log('Dragover');
+    nodes = document.querySelector("#sideNav").querySelector(".side-bar-contents").querySelector("#dropzone").querySelector(".tw-relative").querySelectorAll(".tw-transition");
     whereAmI(ev.clientY);
 });
 
 dropzone.addEventListener("drop", (ev) => {
   ev.preventDefault();
   console.log('Dropped on ' + selectedNodePos);
+  if(selectedNodePos == 1){
+    selectedNodePos = 0;
+  }
   streamsDiv.insertBefore(selectedNode, streamsDiv.children[selectedNodePos]);
-  resetNode();
-
+  selectedNodePos = 0;
 });
 
 
@@ -141,55 +136,35 @@ function establishNodePostions(){
     var yBottom = position.bottom;
     var yCenter = yTop + ((yBottom - yTop) / 2); // distance between top and btm: center
     nodes[i]['yPos'] = yCenter;
+  }
 
     // Tests
   //   console.log(nodes[i]['innderHTML'] + ' is top value of ' + yTop); // Something is wrong with you
   //   console.log(nodes[i]['innderHTML'] + ' is center value of ' + yCenter);
   //   console.log(nodes[i]['innderHTML'] + ' is bottom value of ' + yBottom);
   //   console.log('-----------------');
-   }
 }
 
 function whereAmI(currentYPos){ // There is a problem with the code
   establishNodePostions();
+  
+
    //identify the node that is directly above the selectedNode
    for(var i = 0; i < nodes.length; i++){
-    //console.log(nodes[i]['yPos']);
-    //console.log(currentYPos);
 
-     if (nodes[i]['yPos'] < currentYPos){ // something weird is happening her
-       //This node Must be Higher
-       var nodeAbove = document.getElementById(nodes[i]['id']);
-       selectedNodePos = i + 1;
-      }else {
-        if (!nodeBelow){
-          var nodeBelow = document.getElementById(node[i]['id']);
-        }
+     if (nodes[i]['yPos'] < currentYPos){ 
+        //This node Must be Higher
+        var nodeAbove = document.getElementById(nodes[i]['id']);
+        selectedNodePos = i + 1;
       }
-
     }
     
-     if(typeof nodeAbove == 'undefined'){ // This works
+     if(typeof nodeAbove == undefined){ // This works
        selectedNodePos = 0;
      }
-
-     resetNode();
-
-     if (typeof nodeBelow == 'object'){
-       nodeBelow.style.marginTop = '3em';
-       nodeBelow.style.transition = '1.8s';
-     }
-    
       console.log(selectedNodePos);
-      console.log('------------------');
+    
    }
-
-function resetNode(){
-  for (var i= 0; i < nodes.length; i++ ){
-    document.getElementById(nodes[i]['id']).style.marginTop = '0.5em';
-
-  }
-}
 
 
 
@@ -276,7 +251,7 @@ createFolderButton.onclick = function(){
   streamsDiv.insertBefore(newFolder, streamsDiv.children[0]);
 
 }; 
-controlCenterInfo.appendChild(userTextInput)
+controlCenterInfo.appendChild(userTextInput);
 controlCenterInfo.appendChild(createFolderButton);
 controlCenterSection.appendChild(controlCenterInfo);
 
@@ -284,8 +259,22 @@ controlCenterSection.appendChild(controlCenterInfo);
 //Combine html
 controlCenterBodyDiv.appendChild(controlCenterHeader);
 controlCenterBodyDiv.appendChild(controlCenterSection);
-root.appendChild(controlCenterBodyDiv);
 
-
+// add open button
+var openButtonDiv = document.querySelector("#root").querySelector(".tw-flex").querySelector(".top-nav").querySelector(".tw-justify-content-end");
+var openControlCenterDiv = document.createElement("div");
+openControlCenterDiv.id = "TFOButton";
+openControlCenterDiv.classList.add("tw-align-self-center");
+openControlCenterDiv.classList.add("tw-flex-grow-0");
+openControlCenterDiv.classList.add("tw-flex-nowrap");
+openControlCenterDiv.classList.add("tw-flex-shrink-0");
+openControlCenterDiv.classList.add("tw-mg-x-05");
+var openControlCenterButton = document.createElement("button");
+openControlCenterButton.innerHTML = "TFO";
+openControlCenterDiv.appendChild(openControlCenterButton);
+openButtonDiv.prepend(openControlCenterDiv);
+openControlCenterButton.onclick = function(){
+  root.appendChild(controlCenterBodyDiv);
+  }; 
 
 
